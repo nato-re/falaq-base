d031f85c6272873947021e44ed2a3354abc6f3cf
Pedro Henrique Teixeira <pedroteixeirapimenta03@gmail.com>
2026-09-22 08:23:08 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [EventoController::class, 'index'])->name('eventos.index');
 Route::get('/eventos/{id}', [EventoController::class, 'show'])->name('eventos.show');
-Route::post('/eventos/{id}/perguntas', [EventoController::class, 'storePergunta'])->name('eventos.perguntas.store');
+Route::post('/eventos/{evento}/perguntas', [EventoController::class, 'storePergunta'])
+    ->name('eventos.perguntas.store')
+    ->middleware('auth');
