02a23488b405e581ce7c4a13b366879268455822
Pedro Henrique Teixeira <pedroteixeirapimenta03@gmail.com>
2026-09-22 08:24:03 -0300

    Update EventoController.php

--- app/Http/Controllers/EventoController.php
+++ app/Http/Controllers/EventoController.php
@@ -28,6 +28,7 @@ public function show($id)
     $evento = Evento::find($id);
 
     $perguntas = Pergunta::where('evento_id', $evento->id)
+        ->where('is_public', true)
         ->with('user')
         ->latest()
         ->paginate(10);
